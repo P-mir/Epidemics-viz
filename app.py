4ba57d77526ade96ee3d1cabccca93b9a1f2d8f2
Patrick Guerin <patrick.guerin@businessdecision.be>
2020-05-11 17:12:31 +0200

    up

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+from flask_caching import Cache
 
 import gunicorn
 
@@ -11,11 +12,20 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Epidemics over time"
-server = app.server
+# server = app.server
+
+timeout = 1e20
+
+cache = Cache(app.server, config={
+    # try 'filesystem' if you don't want to setup redis
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': './cache/'
+})
+app.config.suppress_callback_exceptions = True
+
 df = pd.read_csv('data/epidemics.csv', sep=';', encoding='latin1')
 df.dropna(inplace=True)
 df['Death toll'] = df['Death toll'].astype("int32")
-
 
 app.layout = html.Div([
     dcc.Graph(
@@ -29,16 +39,20 @@ app.layout = html.Div([
             value=[df['Date'].min(),df['Date'].max()],
             marks={str(year): str(year) for year in df['Date'].unique() if year%3==0},
             step=2,
-            updatemode='drag'
+            updatemode='mouseup'
+
     )
 ])#, style={'columnCount': 2})
 
+@cache.memoize(timeout=timeout)  # in seconds
 @app.callback(
     Output('graph-with-slider', 'figure'),
     [Input('year-slider', 'value')])
 def update_figure(range):
-    filtered_df = df[(df.Date >= range[0]) & (df.Date <= range[1])]
-    fig = px.treemap(filtered_df, path=['Event'], values='Death toll', color='Disease', title="Epidemic diseases landscape from {} to {}".format(range[0],range[1]))
+    min = range[0]
+    max = range[1]
+    filtered_df = df[(df.Date >= min) & (df.Date <= max)]
+    fig = px.treemap(filtered_df, path=['Event'], values='Death toll', color='Disease', title="Epidemic diseases landscape from {} to {}".format(min, max))
     return fig
 
 
